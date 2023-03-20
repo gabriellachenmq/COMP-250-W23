abcbcb56b50a7af59c81138aff5856835f093538
Gabriella Chen <73800693+gabriellachenmq@users.noreply.github.com>
2023-03-20 06:24:24 -0400

    part 2 actionqueue

--- src/assignment2/ActionQueue.java
+++ src/assignment2/ActionQueue.java
@@ -115,7 +115,7 @@ public class ActionQueue extends MyQueue<Direction> {
                     directionStr = normalStr;
                 }
                 loadFromDirectionsString(directionStr);
-         }
+            }
         }
     }
 
